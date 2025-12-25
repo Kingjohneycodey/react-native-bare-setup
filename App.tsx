b0f4fb75c05f620c9bb6f15eae4bf8b23f71b580
kingjohneycodey <kvngjohnny10@gmail.com>
2025-12-25 06:59:13 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,17 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import "./global.css"
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,7 +13,12 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      {/* <AppContent /> */}
+      <View className="flex-1 items-center justify-center bg-black">
+        <Text className="text-white text-xl font-bold">
+          Bare RN + Tailwind
+        </Text>
+      </View>
     </SafeAreaProvider>
   );
 }
